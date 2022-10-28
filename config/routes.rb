17322c87d0edc2f34539826288f703b2d88a31e5
Chetan Nc <ncchetan@tamu.edu>
2022-10-28 18:19:43 -0500

    Changed database for production

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  Rails.application.routes.draw do
+    resources :movies
+    root :to => redirect('/movies')
+  end
 end
